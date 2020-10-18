1291b9edc8d44fa165102b6649383e7f2c0d63c2
Tiffany Choong <tiffany.choong@rbc.com>
2020-10-17 22:19:07 -0400

    🛠 Start mimikyu; move reusable mixins to separate file

---
+++ pages/art/mimikyu/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="mimikyu">
+		<div class="mimikyu-head">
+			<div class="mimikyu-disguise">
+				<div class="mimikyu-disguise-eye mimikyu-disguise-eye-left"></div>
+				<div class="mimikyu-disguise-eye mimikyu-disguise-eye-right"></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style lang="scss" src="./styles.scss" />
