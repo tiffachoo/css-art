0733c212f9b86859543e3634b795af38e6d5d1bd
Tiffany Choong <tiffany.choong@rbc.com>
2020-10-18 22:20:29 -0400

    🎨 Add disguise features

--- pages/art/mimikyu/index.vue
+++ pages/art/mimikyu/index.vue
@@ -2,8 +2,30 @@
 	<div class="mimikyu">
 		<div class="mimikyu-head">
 			<div class="mimikyu-disguise">
-				<div class="mimikyu-disguise-eye mimikyu-disguise-eye-left"></div>
-				<div class="mimikyu-disguise-eye mimikyu-disguise-eye-right"></div>
+				<div class="mimikyu-disguise-eye mimikyu-disguise-eye-left mimikyu-disguise-line">
+					<div class="mimikyu-disguise-line"></div>
+				</div>
+				<div class="mimikyu-disguise-eye mimikyu-disguise-eye-right mimikyu-disguise-line">
+					<div class="mimikyu-disguise-line mimikyu-disguise-line-1"></div>
+					<div class="mimikyu-disguise-line mimikyu-disguise-line-2"></div>
+				</div>
+				<div class="mimikyu-disguise-blush mimikyu-disguise-blush-left mimikyu-disguise-line">
+					<div class="mimikyu-disguise-line"></div>
+				</div>
+				<div class="mimikyu-disguise-blush mimikyu-disguise-blush-right mimikyu-disguise-line">
+					<div class="mimikyu-disguise-line"></div>
+				</div>
+				<div class="mimikyu-disguise-mouth mimikyu-disguise-mouth-1">
+					<div class="mimikyu-disguise-mouth mimikyu-disguise-mouth-2">
+						<div class="mimikyu-disguise-mouth mimikyu-disguise-mouth-3">
+							<div class="mimikyu-disguise-mouth mimikyu-disguise-mouth-4">
+								<div class="mimikyu-disguise-mouth mimikyu-disguise-mouth-5">
+									<div class="mimikyu-disguise-mouth mimikyu-disguise-mouth-6"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
