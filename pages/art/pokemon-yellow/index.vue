4c70a02f4cb398149cc2b4c3f705f37b466a96d3
Tiffany Choong <tiffany.choong@rbc.com>
2021-02-21 22:46:09 -0500

    ⚡️ Init; Add rough gameboy

---
+++ pages/art/pokemon-yellow/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="container">
+		<div class="gbc">
+			<div class="gbc-screen-wrap">
+				<div class="gbc-screen"></div>
+			</div>
+			<div class="gbc-controls">
+				<div class="gbc-dpad"></div>
+				<div class="gbc-button gbc-button-a"></div>
+				<div class="gbc-button gbc-button-b"></div>
+				<div class="gbc-pill gbc-pill-start"></div>
+				<div class="gbc-pill gbc-pill-select"></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Yellow',
+		head() {
+			return {
+				title: 'yellow',
+			}
+		}
+	}
+</script>
+
+<style lang="scss" src="./styles.scss" />
