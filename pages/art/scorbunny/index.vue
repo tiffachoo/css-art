39edbd713621e1a734970e06f4e74ed900cafe89
Tiffany Choong <tiff.a.choong@gmail.com>
2021-03-29 21:32:22 -0400

    🛠 Initial add of scorbunny; add eyes / head

---
+++ pages/art/scorbunny/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="scor">
+		<div class="scor-head">
+			<div class="scor-face">
+				<div class="scor-eye scor-eye-left"></div>
+				<div class="scor-eye scor-eye-right"></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Scorbunny',
+		head() {
+			return {
+				title: 'Scorbunny',
+			}
+		}
+	}
+</script>
+
+<style lang="scss" src="./styles.scss" />
