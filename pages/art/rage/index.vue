919e9c9635d17c3ebe1e5bb787a0931292f83380
Tiffany Choong <tiff.a.choong@gmail.com>
2021-04-18 17:42:48 -0400

    🛠 Start adding face fur detail

--- pages/art/rage/index.vue
+++ pages/art/rage/index.vue
@@ -4,7 +4,16 @@
 			<div class="retsuko-face">
 				<div class="retsuko-eye retsuko-eye-left"></div>
 				<div class="retsuko-eye retsuko-eye-right"></div>
+				<div class="retsuko-eyebrow retsuko-eyebrow-left"></div>
+				<div class="retsuko-eyebrow retsuko-eyebrow-right"></div>
 				<div class="retsuko-snout">
+					<div class="retsuko-snout-fur-wrap">
+						<div class="retsuko-snout-fur"></div>
+						<div class="retsuko-face-fur retsuko-face-fur-left"></div>
+						<div class="retsuko-face-fur retsuko-face-fur-right"></div>
+						<div class="retsuko-face-fur-white retsuko-face-fur-white-left"></div>
+						<div class="retsuko-face-fur-white retsuko-face-fur-white-right"></div>
+					</div>
 					<div class="retsuko-nose"></div>
 					<div class="retsuko-mouth"></div>
 				</div>
