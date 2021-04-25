393465487364e64f75c6a5d891b6cab6d23a123f
Tiffany Choong <tiff.a.choong@gmail.com>
2021-04-24 21:34:27 -0400

    🛠 Add detail to face fur and ears

--- pages/art/rage/index.vue
+++ pages/art/rage/index.vue
@@ -11,13 +11,23 @@
 						<div class="retsuko-snout-fur"></div>
 						<div class="retsuko-face-fur retsuko-face-fur-left"></div>
 						<div class="retsuko-face-fur retsuko-face-fur-right"></div>
-						<div class="retsuko-face-fur-white retsuko-face-fur-white-left"></div>
-						<div class="retsuko-face-fur-white retsuko-face-fur-white-right"></div>
+						<div class="retsuko-face-fur-white retsuko-face-fur-white-left">
+							<div class="retsuko-face-fur-white-detail retsuko-face-fur-white-detail-1"></div>
+							<div class="retsuko-face-fur-white-detail retsuko-face-fur-white-detail-2"></div>
+							<div class="retsuko-face-fur-white-detail retsuko-face-fur-white-detail-3"></div>
+						</div>
+						<div class="retsuko-face-fur-white retsuko-face-fur-white-right">
+							<div class="retsuko-face-fur-white-detail retsuko-face-fur-white-detail-1"></div>
+							<div class="retsuko-face-fur-white-detail retsuko-face-fur-white-detail-2"></div>
+							<div class="retsuko-face-fur-white-detail retsuko-face-fur-white-detail-3"></div>
+						</div>
 					</div>
 					<div class="retsuko-nose"></div>
 					<div class="retsuko-mouth"></div>
 				</div>
 			</div>
+			<div class="retsuko-ear retsuko-ear-left"></div>
+			<div class="retsuko-ear retsuko-ear-right"></div>
 		</div>
 	</div>
 </template>
