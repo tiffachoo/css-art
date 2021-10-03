9870f1a04cb4039e0b286f8c3774bc61bfd55015
Tiffany Choong <tiff.a.choong@gmail.com>
2021-10-02 22:32:20 -0400

    🛠 Add inner highlight on cutout pieces; wip checkbox

--- pages/art/pumpkin/index.vue
+++ pages/art/pumpkin/index.vue
@@ -1,28 +1,31 @@
 <template>
-	<div class="pumpkin">
-		<div class="pumpkin-body-smol">
-			<div class="pumpkin-body-front"></div>
-			<div class="pumpkin-body-side pumpkin-body-side-left"></div>
-			<div class="pumpkin-body-side pumpkin-body-side-right"></div>
-			<div class="pumpkin-stem">
-				<div class="pumpkin-stem-stick"></div>
-				<div class="pumpkin-stem-base"></div>
+	<div class="vue-container">
+		<input id="pumpkin" type="checkbox" aria-label="Toggle flame" class="pumpkin-control">	
+		<div class="pumpkin">
+			<div class="pumpkin-body-smol">
+				<div class="pumpkin-body-front"></div>
+				<div class="pumpkin-body-side pumpkin-body-side-left"></div>
+				<div class="pumpkin-body-side pumpkin-body-side-right"></div>
+				<div class="pumpkin-stem">
+					<div class="pumpkin-stem-stick"></div>
+					<div class="pumpkin-stem-base"></div>
+				</div>
 			</div>
-		</div>
-		<div class="pumpkin-body">
-			<div class="pumpkin-ear pumpkin-ear-left"></div>
-			<div class="pumpkin-ear pumpkin-ear-right"></div>
-			<div class="pumpkin-face">
-				<div class="pumpkin-eye pumpkin-eye-left"></div>
-				<div class="pumpkin-eye pumpkin-eye-right"></div>
-				<div class="pumpkin-nose"></div>
-				<div class="pumpkin-mouth"></div>
-				<div class="pumpkin-cheek pumpkin-cheek-left"></div>
-				<div class="pumpkin-cheek pumpkin-cheek-right"></div>
+			<div class="pumpkin-body">
+				<div class="pumpkin-ear pumpkin-ear-left"></div>
+				<div class="pumpkin-ear pumpkin-ear-right"></div>
+				<div class="pumpkin-face">
+					<div class="pumpkin-eye pumpkin-eye-left"></div>
+					<div class="pumpkin-eye pumpkin-eye-right"></div>
+					<div class="pumpkin-nose"></div>
+					<div class="pumpkin-mouth"></div>
+					<div class="pumpkin-cheek pumpkin-cheek-left"></div>
+					<div class="pumpkin-cheek pumpkin-cheek-right"></div>
+				</div>
+				<div class="pumpkin-body-front"></div>
+				<div class="pumpkin-body-side pumpkin-body-side-left"></div>
+				<div class="pumpkin-body-side pumpkin-body-side-right"></div>
 			</div>
-			<div class="pumpkin-body-front"></div>
-			<div class="pumpkin-body-side pumpkin-body-side-left"></div>
-			<div class="pumpkin-body-side pumpkin-body-side-right"></div>
 		</div>
 	</div>
 </template>
