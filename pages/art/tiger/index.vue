cdd6a39af46ea529ceaaa4f2a5cdfc3babac0ba9
Tiffany Choong <tiff.a.choong@gmail.com>
2022-01-22 15:16:36 -0500

    🐯 Initial add; wip head

---
+++ pages/art/tiger/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="tiger">
+		<div class="tiger-front">
+			<div class="tiger-head">
+				<div class="tiger-face">
+					<div class="tiger-eye tiger-eye-left"></div>
+					<div class="tiger-eye tiger-eye-right"></div>
+					<div class="tiger-nose"></div>
+					<div class="tiger-snout"></div>
+					<div class="tiger-blush tiger-blush-left"></div>
+					<div class="tiger-blush tiger-blush-right"></div>
+				</div>
+			</div>
+		</div>
+		<div class="tiger-back"></div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'tiger',
+		head() {
+			return {
+				title: 'tiger',
+			}
+		}
+	}
+</script>
+
+<style lang="scss" src="./styles.scss" />
