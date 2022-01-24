041ecd6d6ec741d30266df80fc63eced80917782
Tiffany Choong <tiff.a.choong@gmail.com>
2022-01-23 22:58:37 -0500

    🛠 Add back side; add checkbox flip

--- pages/art/tiger/index.vue
+++ pages/art/tiger/index.vue
@@ -1,36 +1,44 @@
 <template>
-	<div class="tiger">
-		<div class="tiger-front">
-			<div class="tiger-head">
-				<div class="tiger-face">
-					<div class="tiger-eyebrow tiger-eyebrow-left"></div>
-					<div class="tiger-eyebrow tiger-eyebrow-right"></div>
-					<div class="tiger-eye tiger-eye-left"></div>
-					<div class="tiger-eye tiger-eye-right"></div>
-					<div class="tiger-nose"></div>
-					<div class="tiger-snout"></div>
-					<div class="tiger-blush tiger-blush-left"></div>
-					<div class="tiger-blush tiger-blush-right"></div>
-					<div class="tiger-face-fluff tiger-face-fluff-left"></div>
-					<div class="tiger-face-fluff tiger-face-fluff-right"></div>
-					<div class="tiger-face tiger-face-mask">
-						<div class="tiger-stripes-top"></div>
-						<div class="tiger-stripes-side tiger-stripes-side-left"></div>
-						<div class="tiger-stripes-side tiger-stripes-side-right"></div>
+	<div class="vue-container">
+		<input 
+			id="rawr" 
+			aria-label="Flip"
+			class="tiger-control"
+			type="checkbox" 
+		>
+		<label for="rawr" class="tiger">
+			<div class="tiger-front">
+				<div class="tiger-head">
+					<div class="tiger-face">
+						<div class="tiger-eyebrow tiger-eyebrow-left"></div>
+						<div class="tiger-eyebrow tiger-eyebrow-right"></div>
+						<div class="tiger-eye tiger-eye-left"></div>
+						<div class="tiger-eye tiger-eye-right"></div>
+						<div class="tiger-nose"></div>
+						<div class="tiger-snout"></div>
+						<div class="tiger-blush tiger-blush-left"></div>
+						<div class="tiger-blush tiger-blush-right"></div>
+						<div class="tiger-face-fluff tiger-face-fluff-left"></div>
+						<div class="tiger-face-fluff tiger-face-fluff-right"></div>
+						<div class="tiger-face tiger-face-mask">
+							<div class="tiger-stripes-top"></div>
+							<div class="tiger-stripes-side tiger-stripes-side-left"></div>
+							<div class="tiger-stripes-side tiger-stripes-side-right"></div>
+						</div>
+					</div>
+					<div class="tiger-ear tiger-ear-left"></div>
+					<div class="tiger-ear tiger-ear-right"></div>
+				</div>
+				<div class="tiger-front-mask">
+					<div class="tiger-body">
+						<div class="tiger-chest"></div>
 					</div>
 				</div>
-				<div class="tiger-ear tiger-ear-left"></div>
-				<div class="tiger-ear tiger-ear-right"></div>
+				<div class="tiger-paw tiger-paw-left"></div>
+				<div class="tiger-paw tiger-paw-right"></div>
 			</div>
-			<div class="tiger-front-mask">
-				<div class="tiger-body">
-					<div class="tiger-chest"></div>
-				</div>
-			</div>
-			<div class="tiger-paw tiger-paw-left"></div>
-			<div class="tiger-paw tiger-paw-right"></div>
-		</div>
-		<div class="tiger-back"></div>
+			<div class="tiger-back"></div>
+		</label>
 	</div>
 </template>
 
