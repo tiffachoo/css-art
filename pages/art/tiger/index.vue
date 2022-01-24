f2df613d29b097d0ee76e645a8eec4c17ee5cd3d
Tiffany Choong <tiff.a.choong@gmail.com>
2022-01-23 22:15:43 -0500

    🛠 Add face details; add body and paws

--- pages/art/tiger/index.vue
+++ pages/art/tiger/index.vue
@@ -11,11 +11,24 @@
 					<div class="tiger-snout"></div>
 					<div class="tiger-blush tiger-blush-left"></div>
 					<div class="tiger-blush tiger-blush-right"></div>
-					<div class="tiger-stripes-top"></div>
+					<div class="tiger-face-fluff tiger-face-fluff-left"></div>
+					<div class="tiger-face-fluff tiger-face-fluff-right"></div>
+					<div class="tiger-face tiger-face-mask">
+						<div class="tiger-stripes-top"></div>
+						<div class="tiger-stripes-side tiger-stripes-side-left"></div>
+						<div class="tiger-stripes-side tiger-stripes-side-right"></div>
+					</div>
 				</div>
 				<div class="tiger-ear tiger-ear-left"></div>
 				<div class="tiger-ear tiger-ear-right"></div>
 			</div>
+			<div class="tiger-front-mask">
+				<div class="tiger-body">
+					<div class="tiger-chest"></div>
+				</div>
+			</div>
+			<div class="tiger-paw tiger-paw-left"></div>
+			<div class="tiger-paw tiger-paw-right"></div>
 		</div>
 		<div class="tiger-back"></div>
 	</div>
