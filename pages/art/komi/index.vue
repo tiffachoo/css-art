cba7880d20c96e539f69dc9efc6e5f492f8b113f
Tiffany Choong <tiff.a.choong@gmail.com>
2022-05-21 15:41:09 -0400

    🛠 Start komi; wip, head

---
+++ pages/art/komi/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="komi">
+		<div class="komi-head">
+			<div class="komi-hair-back">
+				<div class="komi-hair-back-1"></div>
+			</div>
+			<div class="komi-face"></div>
+			<div class="komi-hair-bangs">
+				<div class="komi-hair-bangs-1"></div>
+				<div class="komi-hair-bangs-2"></div>
+			</div>
+			<div class="komi-face-inner">
+				<div class="komi-eye komi-eye-left"></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'komi',
+		head() {
+			return {
+				title: 'komi',
+			}
+		}
+	}
+</script>
+
+<style lang="scss" src="./styles.scss" />
