33639825198a5e5ba29dd77651c316f9624a9aae
Tiffany Choong <tiff.a.choong@gmail.com>
2022-05-21 23:32:18 -0400

    🛠 Add eyes; start body features

--- pages/art/komi/index.vue
+++ pages/art/komi/index.vue
@@ -4,13 +4,27 @@
 			<div class="komi-hair-back">
 				<div class="komi-hair-back-1"></div>
 			</div>
+			<div class="komi-ear komi-ear-left"></div>
+			<div class="komi-ear komi-ear-right"></div>
 			<div class="komi-face"></div>
 			<div class="komi-hair-bangs">
 				<div class="komi-hair-bangs-1"></div>
 				<div class="komi-hair-bangs-2"></div>
+				<div class="komi-hair-bangs-3"></div>
 			</div>
 			<div class="komi-face-inner">
-				<div class="komi-eye komi-eye-left"></div>
+				<div class="komi-eye komi-eye-left">
+					<div class="komi-eye-pupil"></div>
+				</div>
+				<div class="komi-eye komi-eye-right">
+					<div class="komi-eye-pupil"></div>
+				</div>
+			</div>
+		</div>
+		<div class="komi-panel">
+			<div class="komi-hair-extension"></div>
+			<div class="komi-body">
+				<div class="komi-neck"></div>
 			</div>
 		</div>
 	</div>
