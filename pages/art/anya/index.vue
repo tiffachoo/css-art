27aae5eaf168771b68962beb47ffa7531c760899
Tiffany Choong <tiff.a.choong@gmail.com>
2023-09-03 23:06:42 -0400

    🛠️ Wip, add Anya body and start wing

--- pages/art/anya/index.vue
+++ pages/art/anya/index.vue
@@ -1,12 +1,6 @@
 <template>
 	<div class="anya">
 		<div class="anya-head">
-			<div class="anya-hair">
-				<div class="anya-hair-length anya-hair-length-left">
-					<div class="anya-hair-length-flyaway"></div>
-				</div>
-				<div class="anya-hair-length anya-hair-length-right"></div>
-			</div>
 			<div class="anya-face">
 				<div class="anya-eye anya-eye-left"></div>
 				<div class="anya-eye anya-eye-right"></div>
@@ -19,6 +13,27 @@
 			</div>
 			<div class="anya-hair-front anya-hair-front-left"></div>
 			<div class="anya-hair-front anya-hair-front-right"></div>
+		</div>
+		<div class="anya-head anya-head-back">
+			<div class="anya-hair">
+				<div class="anya-hair-length anya-hair-length-left">
+					<div class="anya-hair-length-flyaway"></div>
+				</div>
+				<div class="anya-hair-length anya-hair-length-right"></div>
+			</div>
+		</div>
+		<div class="anya-body">
+			<div class="anya-wing anya-wing-right"></div>
+			<div class="anya-neck"></div>
+			<div class="anya-dress">
+				<div class="anya-shoulder"></div>
+				<div class="anya-dress-body"></div>
+				<div class="anya-neck-line"></div>
+			</div>
+			<div class="anya-arm anya-arm-left"></div>
+			<div class="anya-arm anya-arm-right"></div>
+			<div class="anya-leg anya-leg-left"></div>
+			<div class="anya-leg anya-leg-right"></div>
 		</div>
 	</div>
 </template>
