674628d06c788959c93f08a681c6e03995ca39d9
Tiffany <tiff.a.choong@gmail.com>
2024-06-23 21:21:37 -0400

    🏄 Initial add; add head and body

---
+++ pages/art/pika-surf/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="pika">
+    <div class="pika-head">
+      <div class="pika-face">
+        <div class="pika-eye pika-eye-left"></div>
+        <div class="pika-eye pika-eye-right"></div>
+        <div class="pika-nose"></div>
+        <div class="pika-mouth">
+          <div class="pika-mouth-outline"></div>
+          <div class="pika-mouth-inner"></div>
+        </div>
+        <div class="pika-cheek pika-cheek-left"></div>
+        <div class="pika-cheek pika-cheek-right"></div>
+      </div>
+      <div class="pika-ear pika-ear-left"></div>
+      <div class="pika-ear pika-ear-right"></div>
+    </div>
+    <div class="pika-body">
+      <div class="pika-torso"></div>
+    </div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'pika-surf',
+		head() {
+			return {
+				title: 'pika-surf',
+			}
+		}
+	}
+</script>
+
+<style lang="scss" src="./styles.scss" />
